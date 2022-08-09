ec5f205f3970ebc52a4c2d9a41ead8347f76c07d
nico-4164 <nicolaschaves1991@gmail.com>
2022-08-09 19:47:09 -0300

    se agregaron Arrays

--- codigo.js
+++ codigo.js
@@ -1,28 +1,52 @@
 
-let duracion;
-let tipo;
+//Arrays
 
+const tipo = ["serie","anime"];
+const duracion = ["corta","larga"];
+
+
+// Clases
+
+class AnimeSerie{
+    constructor(nombre,capitulos,duracion){
+        this.nombre = nombre;
+        this.capitulos = capitulos;
+        this.duracion = duracion;
+    }
+    mostrarInfo(){
+        console.log("Te recomendamos ver "+ this.nombre+ " de "+this.capitulos+" capitulos, con una duracion de "+this.duracion+" horas en total");
+        alert("Te recomendamos ver "+ this.nombre+ " de "+this.capitulos+" capitulos, con una duracion de "+this.duracion+" horas en total");
+    }
+
+};
+
+// Objetos
+
+const animeCorto = new AnimeSerie("Record of Ragnarok","13","4");
+const animeLargo = new AnimeSerie("Mob Psycho 100","26","8");
+
+const serieCorta = new AnimeSerie("Bodyguard","6","6");
+const serieLarga = new AnimeSerie("The Mandalorian","16","10");
+
+
+// Main
 
 function devolverMaraton() {
 
-    tipo=prompt("ingresa un numero para el tipo de contenido que te gustaria ver -> 1=Serie / 2=Anime")
-    duracion=prompt("ingrese un numero para la duracion que quiere para la maraton -> 1=corta / 2=larga");
+    tipoIngresado = prompt("ingresa un numero para el tipo de contenido que te gustaria ver -> 0=Serie / 1=Anime")
+    duracionIngresada = prompt("ingrese un numero para la duracion que quiere para la maraton -> 0=corta / 1=larga");
 
-    if ((duracion == 1) && (tipo == 1)) {
-        console.log("Te recomendamos ver 'Bodyguard' de 6 capitulos, con una duracion de 6 horas en total");
-        alert("Te recomendamos ver 'Bodyguard' de 6 capitulos, con una duracion de 6 horas en total");
+    if ((duracion[duracionIngresada] == "corta") && (tipo[tipoIngresado] == "serie")) {
+        serieCorta.mostrarInfo();
     }
-    else if((duracion == 1) && (tipo == 2)){
-        console.log("Te recomendamos ver 'Another' de 13 capitulos, con una duracion de 4 horas en total");
-        alert("Te recomendamos ver 'Another' de 13 capitulos, con una duracion de 4 horas en total");
+    else if((duracion[duracionIngresada] == "corta") && (tipo[tipoIngresado] == "anime")){
+        animeCorto.mostrarInfo();
     }
-    else if ((duracion == 2) && (tipo == 1)) {
-        console.log("Te recomendamos ver 'The Mandalorian' con 2 temporadas de 8 capitulos C/U, con una duracion de 10 horas en total");
-        alert("Te recomendamos ver 'The Mandalorian' con 2 temporadas de 8 capitulos C/U, con una duracion de 10 horas en total");
+    else if ((duracion[duracionIngresada] == "larga") && (tipo[tipoIngresado] == "serie")) {
+        serieLarga.mostrarInfo();
     }
-    else if ((duracion == 2) && (tipo == 2)) {
-        console.log("Te recomendamos ver 'Mob Psycho 100' con 2 temporadas de 13 capitulos C/U, con una duracion de 8 horas en total");
-        alert("Te recomendamos ver 'Mob Psycho 100' con 2 temporadas de 13 capitulos C/U, con una duracion de 8 horas en total");
+    else if ((duracion[duracionIngresada] == "larga") && (tipo[tipoIngresado] == "anime")) {
+        animeLargo.mostrarInfo();
     }
     else {
         console.log("Uno de los valores ingresado es invalido");
