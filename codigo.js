4ddb9615eff67f0e59f148d1a59700e6a38a3d1f
nico-4164 <nicolaschaves1991@gmail.com>
2022-08-11 15:29:02 -0300

    se suben cambios para primera entrega

--- codigo.js
+++ codigo.js
@@ -1,11 +1,29 @@
 
-//Arrays
 
-const tipo = ["serie","anime"];
-const duracion = ["corta","larga"];
+//Variables y Arrays
+
+let esAnime;
+let esMaratonCorta;
+
+const tiposDeMaraton = ["serie","anime"];
+const duracionesDeMaraton = ["corta","larga"];
+
+let series=[
+    {nombre : "Bodyguard", capitulos:"13", duracion:4},
+    {nombre : "The Mandalorian", capitulos:"16", duracion:12},
+    {nombre : "Mindhunter temporada 1", capitulos:"10", duracion:10},
+    {nombre : "Chernobyl", capitulos:"5", duracion:5}
+]
+
+let animes=[
+    {nombre : "Mob Psycho 100", capitulos:"26", duracion:8},
+    {nombre : "Death Note", capitulos:"37", duracion:12},
+    {nombre : "Record of Ragnarok", capitulos:"13", duracion:4},
+    {nombre : "Death Parade", capitulos:"13", duracion:4}
+]
 
 
-// Clases
+// Clases y Objetos
 
 class AnimeSerie{
     constructor(nombre,capitulos,duracion){
@@ -20,38 +38,56 @@ class AnimeSerie{
 
 };
 
-// Objetos
-
-const animeCorto = new AnimeSerie("Record of Ragnarok","13","4");
-const animeLargo = new AnimeSerie("Mob Psycho 100","26","8");
-
-const serieCorta = new AnimeSerie("Bodyguard","6","6");
-const serieLarga = new AnimeSerie("The Mandalorian","16","10");
 
 
-// Main
+//Funciones
+
+function tipoMaraton(tipoMaraton) {
+    return tiposDeMaraton[tipoMaraton] == "anime";
+}
+
+function duracionMaraton(duracionMaraton) {
+    return duracionesDeMaraton[duracionMaraton] == "corta";
+}
+
+
+
+// Funcion Principal
 
 function devolverMaraton() {
 
-    tipoIngresado = prompt("ingresa un numero para el tipo de contenido que te gustaria ver -> 0=Serie / 1=Anime")
-    duracionIngresada = prompt("ingrese un numero para la duracion que quiere para la maraton -> 0=corta / 1=larga");
+    let maraton;
+    let tipo=prompt("ingresa un numero para el tipo de contenido que te gustaria ver -> 0=Serie / 1=Anime"); 
+    let duracion=prompt("ingrese un numero para la duracion que quiere para la maraton -> 0=corta / 1=larga");
 
-    if ((duracion[duracionIngresada] == "corta") && (tipo[tipoIngresado] == "serie")) {
-        serieCorta.mostrarInfo();
+    //primero me fijo que los datos ingredaos sean validos
+    if (tipo > 1 || duracion > 1) {
+        return alert("Uno de los valores ingresado es invalido");
     }
-    else if((duracion[duracionIngresada] == "corta") && (tipo[tipoIngresado] == "anime")){
-        animeCorto.mostrarInfo();
+
+
+    //si los datos son validos, asignos los valores a las variables y las uso para encontrar una serie o anime para ver
+    esAnime=tipoMaraton(tipo);
+    esMaratonCorta=duracionMaraton(duracion);
+
+    if (esAnime) {
+        if (esMaratonCorta) {
+            maraton = animes.find((anime) => anime.duracion <= 5)
+        }else{
+            maraton = animes.find((anime) => anime.duracion > 5)
+        }
     }
-    else if ((duracion[duracionIngresada] == "larga") && (tipo[tipoIngresado] == "serie")) {
-        serieLarga.mostrarInfo();
+    else{
+        if (esMaratonCorta) {
+            maraton = series.find((anime) => anime.duracion <= 5)
+        }else{
+            maraton = series.find((anime) => anime.duracion > 5)
+        }
     }
-    else if ((duracion[duracionIngresada] == "larga") && (tipo[tipoIngresado] == "anime")) {
-        animeLargo.mostrarInfo();
-    }
-    else {
-        console.log("Uno de los valores ingresado es invalido");
-        alert("Uno de los valores ingresado es invalido");
-    }
+
+    //con los valores de la serie o anime creo un objeto maraton para que mostrar los datos
+    let maratonCreada = new AnimeSerie(maraton.nombre,maraton.capitulos,maraton.duracion);
+    maratonCreada.mostrarInfo();
 
 }
 
