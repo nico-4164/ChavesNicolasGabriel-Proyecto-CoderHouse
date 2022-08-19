23e921e7b1a6fd2265a602e06e744d49a4ebe2fe
nico-4164 <nicolaschaves1991@gmail.com>
2022-08-19 00:04:08 -0300

    se suben cambios para entrega

--- codigo.js
+++ codigo.js
@@ -1,29 +1,46 @@
 
 
-//Variables y Arrays
+////////////////    Variables y Arrays   //////////////////////
 
-let esAnime;
-let esMaratonCorta;
+console.dir(document.body);
 
-const tiposDeMaraton = ["serie","anime"];
-const duracionesDeMaraton = ["corta","larga"];
+let tipo="";
+let duracionMinima=0;
+let duracionMaxima=15;
 
-let series=[
+let inputTipo = document.getElementById("tipo_maraton");
+let inputDuracionMinima = document.getElementById("duracion_minima");
+let inputDuracionMaxima = document.getElementById("duracion_maxima");
+
+inputTipo.onchange = () => {tipo = inputTipo.value};
+inputDuracionMinima.onchange = () => {duracionMinima = inputDuracionMinima.value};
+inputDuracionMaxima.onchange = () => {duracionMaxima = inputDuracionMaxima.value};
+
+let listaSeries=[
     {nombre : "Bodyguard", capitulos:"13", duracion:4},
     {nombre : "The Mandalorian", capitulos:"16", duracion:12},
     {nombre : "Mindhunter temporada 1", capitulos:"10", duracion:10},
+    {nombre : "The Boys temporada 1", capitulos:"8", duracion:8},
+    {nombre : "Stranger Things temporada 1", capitulos:"8", duracion:7},
+    {nombre : "Mr Robot temporada 1", capitulos:"10", duracion:8},
+    {nombre : "Rick And Morty temporada 1", capitulos:"11", duracion:4},
     {nombre : "Chernobyl", capitulos:"5", duracion:5}
 ]
 
-let animes=[
+let ListaAnimes=[
+    {nombre : "Fullmetal Alchemist: Brotherhood", capitulos:"64", duracion:21},
     {nombre : "Mob Psycho 100", capitulos:"26", duracion:8},
     {nombre : "Death Note", capitulos:"37", duracion:12},
     {nombre : "Record of Ragnarok", capitulos:"13", duracion:4},
+    {nombre : "Dr. Stone", capitulos:"24", duracion:8},
+    {nombre : "Arakawa Under the Bridge", capitulos:"13", duracion:4},
+    {nombre : "Bakuman", capitulos:"25", duracion:8},
+    {nombre : "Kill la Kill", capitulos:"24", duracion:8},
     {nombre : "Death Parade", capitulos:"13", duracion:4}
 ]
 
-
-// Clases y Objetos
+/////////////////////////////////////////////////////////////
+////////////////    Clases y Objetos   //////////////////////
 
 class AnimeSerie{
     constructor(nombre,capitulos,duracion){
@@ -31,63 +48,98 @@ class AnimeSerie{
         this.capitulos = capitulos;
         this.duracion = duracion;
     }
+
     mostrarInfo(){
-        console.log("Te recomendamos ver "+ this.nombre+ " de "+this.capitulos+" capitulos, con una duracion de "+this.duracion+" horas en total");
-        alert("Te recomendamos ver "+ this.nombre+ " de "+this.capitulos+" capitulos, con una duracion de "+this.duracion+" horas en total");
+
+        let mensaje = document.getElementById("maraton_mensaje");
+        let titulo = document.getElementById("maraton_titulo");
+        let capitulos = document.getElementById("maraton_capitulos");
+        let duracion = document.getElementById("maraton_duracion");
+
+        mensaje.innerText = "TE RECOMENDAMOS VER"
+        titulo.innerText = this.nombre;
+        capitulos.innerText = "Capitulos: " + this.capitulos;
+        duracion.innerText = "Duracion: " + this.duracion + " Horas aproximadamente";
+    }
+
+    tieneDuracionPedida(min, max){
+        if ((this.duracion >= min) && (this.duracion < max)) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
 };
 
 
+//////////////////////////////////////////////////////
+////////////////    Funciones   //////////////////////
 
-//Funciones
-
-function tipoMaraton(tipoMaraton) {
-    return tiposDeMaraton[tipoMaraton] == "anime";
+function filtrarPorTipo(tipo) {
+    switch (tipo) {
+        case "serie":
+            return listaSeries;
+        case "anime":
+            return ListaAnimes;
+        default:
+            break;
+    }
 }
 
-function duracionMaraton(duracionMaraton) {
-    return duracionesDeMaraton[duracionMaraton] == "corta";
+function filtarPoDuracion(lista, min , max) {
+
+    let maratonRespuesta=[];
+
+    for(const contenido of lista){
+        console.log(contenido)
+        if ((contenido.duracion >= min) && (contenido.duracion < max)) {
+            maratonRespuesta.push(contenido);
+        }
+    }
+    console.log("log de la lista despues de buscar las duraciones");
+    console.log(maratonRespuesta);
+
+    return maratonRespuesta;
 }
 
-
-
-// Funcion Principal
+//////////////////////////////////////////////////////////////
+////////////////    Funcion Principal   //////////////////////
 
 function devolverMaraton() {
 
-    let maraton;
-    let tipo=prompt("ingresa un numero para el tipo de contenido que te gustaria ver -> 0=Serie / 1=Anime"); 
-    let duracion=prompt("ingrese un numero para la duracion que quiere para la maraton -> 0=corta / 1=larga");
+    let maraton=[];
+
+    console.log("maraton al inicio ");
+    console.log(maraton);
 
     //primero me fijo que los datos ingredaos sean validos
-    if (tipo > 1 || duracion > 1) {
-        return alert("Uno de los valores ingresado es invalido");
+    if (duracionMinima.value >= duracionMaxima.value) {
+        return alert("Los valores ingresados para la duracion de la maraton son invalidos");
     }
+
 
 
     //si los datos son validos, asignos los valores a las variables y las uso para encontrar una serie o anime para ver
-    esAnime=tipoMaraton(tipo);
-    esMaratonCorta=duracionMaraton(duracion);
 
-    if (esAnime) {
-        if (esMaratonCorta) {
-            maraton = animes.find((anime) => anime.duracion <= 5)
-        }else{
-            maraton = animes.find((anime) => anime.duracion > 5)
-        }
-    }
-    else{
-        if (esMaratonCorta) {
-            maraton = series.find((anime) => anime.duracion <= 5)
-        }else{
-            maraton = series.find((anime) => anime.duracion > 5)
-        }
-    }
+    maraton = filtrarPorTipo(tipo);
+    console.log("maraton despues de buscar por tipos");
+    console.log(maraton);
+    maraton = filtarPoDuracion(maraton, duracionMinima, duracionMaxima);
+    console.log("maraton despues de buscar por duracion");
+    console.log(maraton);
+
 
     //con los valores de la serie o anime creo un objeto maraton para que mostrar los datos
-    let maratonCreada = new AnimeSerie(maraton.nombre,maraton.capitulos,maraton.duracion);
-    maratonCreada.mostrarInfo();
+    if (maraton.length == 0) {
+        alert("no a sido posible encontrar una maraton con las opciones seleccionadas")
+    }else{
+        let maratonCreada = new AnimeSerie(maraton[0].nombre,maraton[0].capitulos,maraton[0].duracion);
+        maratonCreada.mostrarInfo();
+    }
+    
 
 }
+
+
 
